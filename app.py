1993b3ac982758a858e7798c018ff9bb5a0d855f
B3RN4R-1022 <64097737+B3RN4R-1022@users.noreply.github.com>
2023-09-30 14:28:39 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,86 @@
+
+from flask import Flask, render_template, request, jsonify, send_from_directory
+import openai
+import os
+
+app = Flask(__name__)
+
+# Configure a sua chave de API da OpenAIF
+openai.api_key = "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"
+
+# Carrega os prompts de arquivos
+PROMPT_DIR = os.path.join(os.getcwd(), 'prompts')
+prompt_files = os.listdir(PROMPT_DIR)
+prompts = {}
+for file in prompt_files:
+    if file.endswith('.txt'):
+        with open(os.path.join(PROMPT_DIR, file), 'r') as f:
+            content = f.read()
+            prompts[file] = content
+
+@app.route('/componentes/fonts/<font_file>')
+def servir_arquivo_fonte(font_file):
+    return send_from_directory('componentes/fonts', font_file)
+
+@app.route('/image/<nome_da_imagem>')
+def servir_imagem(nome_da_imagem):
+    return send_from_directory('image', nome_da_imagem)
+
+# Rota para a página TAB
+@app.route('/pagina-tab')
+def pagina_tab():
+    return render_template('pagina_tab.html')
+
+# Rota para a página DOC
+
+
+@app.route('/pagina-doc')
+def pagina_doc():
+    return render_template('pagina_doc.html')
+
+# Rota para a página PROMP
+
+
+@app.route('/pagina-prompt')
+def pagina_prompt():
+    return render_template('pagina_prompt.html')
+
+# Rota para a página GE
+
+
+@app.route('/pagina-ge')
+def pagina_ge():
+    return render_template('pagina_ge.html')
+
+# Rota para a página HOME
+
+
+@app.route('/pagina-home')
+def pagina_home():
+    return render_template('pagina_home.html')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', prompts=prompts)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    prompt = request.form['prompt']
+    user_input = request.form['user_input']
+    print("Prompt recebido:", prompt)
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"{prompt} {user_input}",
+        temperature=0.7,
+        max_tokens=1500,
+        n=1,
+        stop=None
+    )
+    assistant_response = response.choices[0].text.strip()
+    return jsonify({'response': assistant_response})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/scripts.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const apiKeyIcon = document.getElementById("api-key-icon");
+    const apiKeyInput = document.getElementById("api-key-input");
+
+    apiKeyIcon.addEventListener("click", function () {
+        const apiKey = prompt("Digite sua chave API:");
+        if (apiKey !== null) {
+            apiKeyInput.value = apiKey;
+        }
+    });
+
+    const promptSelect = document.getElementById("prompt-selector");
+    const userInput = document.getElementById("user-input");
+    const submitButton = document.getElementById("arrow-icon");
+    const conversationContainer = document.querySelector(".conversation-container");
+
+    submitButton.addEventListener("click", async function () {
+        const selectedPrompt = promptSelect.value;
+        const userMessage = userInput.value;
+
+        if (userMessage.trim() !== "") {
+            appendMessage("user-message", userMessage);
+
+            const response = await fetch("/submit", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: `prompt=${selectedPrompt}&user_input=${userMessage}`
+            });
+
+            const responseData = await response.json();
+            const assistantResponse = responseData.response;
+
+            appendMessage("assistant-message", assistantResponse);
+
+            userInput.value = "";
+            conversationContainer.scrollTop = conversationContainer.scrollHeight;
+        }
+    });
+
+
+    function appendMessage(className, message) {
+        const messageElement = document.createElement("div");
+        messageElement.classList.add("message", className);
+        messageElement.textContent = message;
+        conversationContainer.appendChild(messageElement);
+    }
+
+    userInput.addEventListener("keydown", function(event) {
+        // Verifica se a tecla pressionada foi Enter e se não está pressionando a tecla Shift junto
+        if (event.key === "Enter" && !event.shiftKey) {
+            event.preventDefault(); // Evita a ação padrão do Enter (por exemplo, quebra de linha)
+            
+            // Aqui, chame a função que envia a mensagem
+            // Por exemplo, se você tem um botão que envia a mensagem quando clicado:
+            submitButton.click();
+        }
+    });
+});
